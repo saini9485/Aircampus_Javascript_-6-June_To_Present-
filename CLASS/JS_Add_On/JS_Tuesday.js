df10946e7b320b097a63aafe047c3d58a7fa8dd5
Rajesh <saini222302@gmail.com>
2022-06-29 02:02:12 +0530

    add question

--- CLASS/JS_Add_On/JS_Tuesday.js
+++ CLASS/JS_Add_On/JS_Tuesday.js
@@ -1,17 +1,66 @@
-getName()
-console.log(fname)
-console.log(lname)
+// getName()
+// console.log(fname)
+// console.log(lname)
 
-let lname = 'vasanth'
-var fname = 'kumar'
-function getName(){
-    printName()
-    console.log('calling')
-    function printName(){
-        console.log(place);
-        let age = 10;
-        console.log(age)
-        var place = 'chennai'
-    }
+// let lname = 'vasanth'
+// var fname = 'kumar'
+// function getName(){
+//     printName()
+//     console.log('calling')
+//     function printName(){
+//         console.log(place);
+//         let age = 10;
+//         console.log(age)
+//         var place = 'chennai'
+//     }
 
+// }
+//10.
+/*let i,j,k;
+for(i=1;i<=5;i++){
+stars = ''
+for(j=1;j<=5-i;j++){
+  stars += ' '
 }
+for(k=1;k<=i;k++){
+  stars +=`${k}`;
+}
+console.log(stars)
+}*/
+//13.
+/*let i,j;
+for(i=1;i<=4;i++){
+let stars = ''
+  for(j=1;j<=4;j++){
+    if(i==1||i==4||j==1||j==4){
+      stars += '*'
+    } else{
+      stars +=' '
+    }
+  }
+  console.log(stars)
+}*/
+//11.
+/*let i,j,k;
+for(i=1;i<=5;i++){
+stars = ''
+for(j=1;j<=5-i;j++){
+  stars += ' '
+}
+for(k=1;k<=i;k++){
+  stars +='* ';
+}
+console.log(stars)
+}*/
+//12.
+/*let i,j,k;
+for(i=1;i<=5;i++){
+stars = ''
+for(j=1;j<=5-i;j++){
+  stars += ' '
+}
+for(k=1;k<=i;k++){
+  stars +=k*2;
+}
+console.log(stars)
+}*/
---
+++ CLASS/JS_Add_On/JS_Tuesday/JS_Tuesday.js
@@ -0,0 +1,17 @@
+getName()
+console.log(fname)
+console.log(lname)
+
+let lname = 'vasanth'
+var fname = 'kumar'
+function getName(){
+    printName()
+    console.log('calling')
+    function printName(){
+        console.log(place);
+        let age = 10;
+        console.log(age)
+        var place = 'chennai'
+    }
+
+}
