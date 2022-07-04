fa01516742ca4db4badf44212dae559c78fd6e2b
Rajesh <saini222302@gmail.com>
2022-07-04 22:08:17 +0530

    Pizza

---
+++ JS_Test/Weekly_Test_JS_3/Q.6_Pizza.js
@@ -0,0 +1,33 @@
+function inhotel (Callback){
+    Callback();
+  }
+   function orderfood (){
+    console.log(`step1:order a pizza`);
+  }
+  function processing (){
+    console.log(`step2: pizza order a pizza`);
+  }
+  function ingredeinats (){
+    console.log(`step3: ingredeinats ready`);
+  }
+  function falvaoured (){
+    console.log(`step4:falvouerd are added`);
+  }
+  function cooked (){
+    console.log(`step15: cooked and packed`);
+  }
+  function delivery (){
+    console.log(`step6: ready for dispatched`);
+  }
+  function pizza (){
+    console.log(`step7:pizza received and ate `);
+  }
+  
+  inhotel ( orderfood  )
+  inhotel (  processing )
+  inhotel ( ingredeinats )
+  inhotel ( falvaoured  )
+  inhotel (cooked)
+  inhotel ( delivery )
+  inhotel (  pizza  )
+  
