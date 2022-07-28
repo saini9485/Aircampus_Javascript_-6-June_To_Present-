30da21b187beb23582930f4357a44510f909cc8c
Rajesh <saini222302@gmail.com>
2022-07-28 09:15:06 +0530

    basic

--- CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
+++ CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
@@ -14,17 +14,28 @@
 // }
 //operator 
 //1. "+" operator
-console.log(10+20)//30
-console.log("10"+20)//1020//beacouse plus be use for string add
-console.log(10+"20")//1020 //
-console.log("10"+"20")//1020
-console.log("hello"+20)//hello20
+// console.log(10+20)//30
+// console.log("10"+20)//1020//beacouse plus be use for string add
+// console.log(10+"20")//1020 //it will different only + operator
+// console.log("10"+"20")//1020
+// console.log("hello"+20)//hello20
 //2."-" operator
-console.log(20-10)//10
-console.log("20"-10)//10
-console.log("20"-"10")//10
-console.log(20-"hello")//NaN
-console.log("hello"-"10")//NaN
+// console.log(20-10)//10
+// console.log("20"-10)//10
+// console.log("20"-"10")//10
+// console.log(20-"hello")//NaN
+// console.log("hello"-"10")//NaN
 //3."*" operator
-console.log(20-"10")//10
-console.log(20-"10")//10
+// console.log(20*10)//200
+// console.log("20"*10)//200
+// console.log("20"*"10")//200
+// console.log("hello"*10)//NaN
+//console.log(hello*10)//error
+//4. "/" operator
+console.log(20/10)//2
+console.log("20"/10)//2
+console.log(20/"10")//2
+console.log("20"/"10")//2
+console.log("hello"/10)//NaN
+console.log("Hello"/"10")//NaN
+console.log("Hello"/"hello")//NaN
