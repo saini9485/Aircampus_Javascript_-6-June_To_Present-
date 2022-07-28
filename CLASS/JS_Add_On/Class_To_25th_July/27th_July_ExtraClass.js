aaa7974f5f3cc896a15ed3b7944e0055c7601290
Rajesh <saini222302@gmail.com>
2022-07-28 09:58:10 +0530

    basic

--- CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
+++ CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
@@ -12,6 +12,10 @@
 // }else {
 //     console.log("no") 
 // }
+//javascript is single threaded language
+// single threaded means it will read line by line 
+//multi threaded means it will read more than one line in one time 
+
 //operator 
 //1. "+" operator
 // console.log(10+20)//30
@@ -32,10 +36,50 @@
 // console.log("hello"*10)//NaN
 //console.log(hello*10)//error
 //4. "/" operator
-console.log(20/10)//2
-console.log("20"/10)//2
-console.log(20/"10")//2
-console.log("20"/"10")//2
-console.log("hello"/10)//NaN
-console.log("Hello"/"10")//NaN
-console.log("Hello"/"hello")//NaN
+// console.log(20/10)//2
+// console.log("20"/10)//2
+// console.log(20/"10")//2
+// console.log("20"/"10")//2
+// console.log("hello"/10)//NaN
+// console.log("Hello"/"10")//NaN
+// console.log("Hello"/"hello")//NaN
+//5. "!" operator
+//falsy value
+//false , 0 , "",NaN, null, undefine
+console.log(0)//0
+console.log(!0)//True
+console.log(!!0)//false
+console.log(0)//
+console.log("")// empty
+console.log(!"") //True
+console.log(!!"") //false
+console.log(!NaN) //True
+console.log(!!NaN) //false
+console.log(!null) //True
+console.log(!!null) //false
+console.log(!undefine) //true
+console.log(!!undefine) //false
+// Variable :-
+//what is variable 
+//variable is a container where we store the value
+//1.var :-we can reassign and redeclear the value
+//2. let :- we can reassign value but can not redeclear value
+//3. const :- we can not reassign and can not redeclear value both
+//why it is called variable :-
+//because this value be varry
+//what is datatype;-
+//there are two datatype 
+//1.primitive datatype
+//a.number => 1,2,3
+//b.String => "hello"
+//c.boolean  => true or false
+//d.Undefine => let a;
+//c.Symbols  => let value = symbol('hello')
+//d.null  => let a = null;
+//2.non primitive or reference data type
+//a.function => function (){}
+//b.String   => "hello"
+//c.array  => [1,2,3]
+//d.Object  {}
+
+
