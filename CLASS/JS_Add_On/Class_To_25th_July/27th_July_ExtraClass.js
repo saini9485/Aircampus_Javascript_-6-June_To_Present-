794d0fbecb95345e9655351a8d115b1384f5f220
Rajesh <saini222302@gmail.com>
2022-07-28 08:05:44 +0530

    Basic

--- CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
+++ CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
@@ -1,5 +1,13 @@
+//find the good is present or not in given string 
+// let news = "vasanth is a good teacher";
+//  if(news.includes ("good")){
+//     console.log("yes") 
+// }else {
+//     console.log("no") 
+// }
+//find on 5th number n is present or not in given string 
 let news = "vasanth is a good teacher";
- if(news.includes =="is"){
+ if(news[4]=="n"){
     console.log("yes") 
 }else {
     console.log("no") 
