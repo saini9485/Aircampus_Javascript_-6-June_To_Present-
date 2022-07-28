0472c0a135c77fe8db9d25ef33fbc7c90e0affba
Rajesh <saini222302@gmail.com>
2022-07-28 08:52:05 +0530

    basic

--- CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
+++ CLASS/JS_Add_On/Class_To_25th_July/27th_July_ExtraClass.js
@@ -6,9 +6,25 @@
 //     console.log("no") 
 // }
 //find on 5th number n is present or not in given string 
-let news = "vasanth is a good teacher";
- if(news[4]=="n"){
-    console.log("yes") 
-}else {
-    console.log("no") 
-}
+// let news = "vasanth is a good teacher";
+//  if(news[4]=="n"){
+//     console.log("yes") 
+// }else {
+//     console.log("no") 
+// }
+//operator 
+//1. "+" operator
+console.log(10+20)//30
+console.log("10"+20)//1020//beacouse plus be use for string add
+console.log(10+"20")//1020 //
+console.log("10"+"20")//1020
+console.log("hello"+20)//hello20
+//2."-" operator
+console.log(20-10)//10
+console.log("20"-10)//10
+console.log("20"-"10")//10
+console.log(20-"hello")//NaN
+console.log("hello"-"10")//NaN
+//3."*" operator
+console.log(20-"10")//10
+console.log(20-"10")//10
