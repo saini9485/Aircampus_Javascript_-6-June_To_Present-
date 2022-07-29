c70705ac2a2ae0c04134ba5c63db60ff7304591a
Rajesh <saini222302@gmail.com>
2022-07-29 09:22:52 +0530

    DOM

--- CLASS/JS_Add_On/Class_To_25th_July/28th_July_DOM.js
+++ CLASS/JS_Add_On/Class_To_25th_July/28th_July_DOM.js
@@ -1,1 +1,42 @@
-const grandParent = document.querySelector(".grandparent")
+const grandParent = document.querySelector(".grandparent");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+const btn = document.querySelector(".btn");
+
+function printGrandParent(event) {
+  console.log("grand parent bubbling", event.target);
+}
+
+function printOnHover() {
+  console.log("hovering");
+}
+
+grandParent.addEventListener("click", printGrandParent);
+
+function removeGrandParent() {
+  setTimeout(() => {
+    console.log("removing grandparent");
+    grandParent.removeEventListener("click", printGrandParent);
+  }, 2000);
+}
+
+btn.addEventListener("click", removeGrandParent);
+
+parent.addEventListener("click", (event) => {
+  console.log("parent bubbling");
+});
+
+child.addEventListener("click", (event) => {
+  console.log("child bubbling");
+});
+
+const newDiv = document.createElement("div");
+newDiv.style.width = "10rem";
+newDiv.style.height = "10rem";
+newDiv.style.backgroundColor = "red";
+const para = document.createElement("p");
+para.innerText = "vasanthkumar ";
+newDiv.append(para);
+document.body.append(newDiv);
+
+child.append(para);
--- CLASS/JS_Add_On/Class_To_25th_July/28th_July_ExtraClass.js
+++ CLASS/JS_Add_On/Class_To_25th_July/28th_July_ExtraClass.js
@@ -4,6 +4,7 @@
 //     console.log("greater")
 // }else{
 //
+//find the even number to the array
     console.log("smaller")
 let arr = [10,2,3,4,1,7,8]
  let even = arr.filter(number=>{
