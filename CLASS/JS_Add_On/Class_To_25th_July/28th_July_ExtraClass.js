f27121fa93e6e9854427c81291294a02a5049c46
Rajesh <saini222302@gmail.com>
2022-07-29 19:08:34 +0530

    basic

--- CLASS/JS_Add_On/Class_To_25th_July/28th_July_ExtraClass.js
+++ CLASS/JS_Add_On/Class_To_25th_July/28th_July_ExtraClass.js
@@ -1,13 +1,23 @@
-// let names = "vasanthkumar"
+// let names = "Vasanthkumar"
 
 // if(names.length>10){
 //     console.log("greater")
 // }else{
-//
 //find the even number to the array
-    console.log("smaller")
-let arr = [10,2,3,4,1,7,8]
- let even = arr.filter(number=>{
-    return number%2==0;
- });
- console.log(even)
+//     console.log("smaller")
+// let arr = [10,2,3,4,1,7,8]
+//  let even = arr.filter(number=>{
+//     return number%2==0;
+//  });
+//  console.log(even)
+//function declaration and function expression
+function declaration (){
+    console.log("declaration")
+} declaration()
+
+const expression = function  (){
+    console.log("expression")
+}
+ expression()
+
+
